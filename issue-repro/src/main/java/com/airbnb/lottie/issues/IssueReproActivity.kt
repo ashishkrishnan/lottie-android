2e3076ef8f52ccbb858776c4c6ecdd671b5ffb1f
Ashish Krishnan <ashishkrish09@gmail.com>
2021-05-09 19:34:58 +0530

    Adds a barebone permission request setup for Location.

--- issue-repro/src/main/java/com/airbnb/lottie/issues/IssueReproActivity.kt
+++ issue-repro/src/main/java/com/airbnb/lottie/issues/IssueReproActivity.kt
@@ -1,7 +1,11 @@
 package com.airbnb.lottie.issues
 
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.content.pm.PackageManager.PERMISSION_DENIED
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.airbnb.lottie.issues.databinding.IssueReproActivityBinding
 
 class IssueReproActivity : AppCompatActivity() {
@@ -10,5 +14,16 @@ class IssueReproActivity : AppCompatActivity() {
         val binding = IssueReproActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
         // Reproduce any issues here.
+        requestPermission()
+    // Bare bone implementation of requesting permission
+    private fun requestPermission() {
+        if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) == PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this, arrayOf(ACCESS_FINE_LOCATION), 100)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        Toast.makeText(this, grantResults.toString(), Toast.LENGTH_LONG).show()
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 }
